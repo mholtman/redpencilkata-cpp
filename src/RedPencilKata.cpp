d7eea5c4846c636c33b048eff6bc6d518c40c2c4
Mark Holtman <mholtman@gmail.com>
2012-10-22 11:00:47 -0400

    stable test working

--- src/RedPencilKata.cpp
+++ src/RedPencilKata.cpp
@@ -2,11 +2,15 @@
 
 #include "boost/date_time/posix_time/posix_time.hpp"
 
+using namespace boost::posix_time;
+using namespace boost::gregorian;
+
 RedPencilItem::RedPencilItem(int price){
     priceInCents = new int;
     *priceInCents = price;
     modifiedPriceInCents = new int;
     *modifiedPriceInCents = price;
+    
 }
 
 RedPencilItem::~RedPencilItem(){
@@ -26,9 +30,22 @@ bool RedPencilItem::IsStable() {
   using namespace boost::posix_time;
   using namespace boost::gregorian;
 
-  ptime now = second_clock::local_time();
+   ptime now = second_clock::local_time();
   date today = now.date();
+  date_duration ddEndPromo(30);
+  date stableDate = today + ddEndPromo;
   
+  if (!promoEndDate)
+  {
+    return true;
+  }
+  else if (stableDate <= promoEndDate)
+  {
+    return true
+  }
   
- return true;
+  return false;
+  
+ 
 }
+ 
--- src/RedPencilKata.h
+++ src/RedPencilKata.h
@@ -1,8 +1,15 @@
 #include "boost/date_time/posix_time/posix_time.hpp"
 
+using namespace boost::posix_time;
+using namespace boost::gregorian;
+
 class RedPencilItem {
+
+  using namespace boost::posix_time;
+  using namespace boost::gregorian;
   int *priceInCents;
   int *modifiedPriceInCents;
+  date *promoEndDate;
 public:
   RedPencilItem(int);
   ~RedPencilItem();
